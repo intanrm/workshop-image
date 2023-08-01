909a072668c652963c94062faf637fa8ea7228ed
Intan Rismawati <89776494+intanrm@users.noreply.github.com>
2023-08-01 08:09:56 +0700

    Add files via upload

---
+++ IntanForm/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Button1 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(27, 22)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(192, 284)
+        PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Location = New Point(263, 22)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(192, 284)
+        PictureBox2.SizeMode = PictureBoxSizeMode.AutoSize
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(73, 355)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 17F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ IntanForm/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.Windows.Forms
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        ' Set the image file filter to allow only image files
+        openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp"
+
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim originalImage As New Bitmap(openFileDialog1.FileName)
+
+            ' Create a new bitmap with the same dimensions as the original image
+            Dim modifiedImage As New Bitmap(originalImage.Width, originalImage.Height)
+
+            ' Define the five different colors with reduced opacity
+            Dim colors() As Color = {Color.FromArgb(100, Color.Red), Color.FromArgb(100, Color.Blue), Color.FromArgb(100, Color.Green), Color.FromArgb(100, Color.Yellow), Color.FromArgb(100, Color.Purple)}
+
+            ' Calculate the width for each color band
+            Dim bandWidth As Integer = originalImage.Width \ colors.Length
+
+            ' Draw the original image on the modified image
+            Using graphics As Graphics = Graphics.FromImage(modifiedImage)
+                graphics.DrawImage(originalImage, 0, 0)
+
+                ' Draw the bands with different colors and reduced opacity on the modified image
+                For i As Integer = 0 To colors.Length - 1
+                    Dim startX As Integer = i * bandWidth
+                    Dim endX As Integer = (i + 1) * bandWidth
+
+                    Dim brush As New SolidBrush(colors(i))
+                    graphics.FillRectangle(brush, startX, 0, endX - startX, originalImage.Height)
+                Next
+            End Using
+
+            ' Display the original and modified images in the PictureBox controls
+            PictureBox1.Image = originalImage
+            PictureBox2.Image = modifiedImage
+
+            ' Save the result image
+            Dim savePath As String = "result.png"
+            modifiedImage.Save(savePath, ImageFormat.Png)
+
+            MsgBox("Result image saved successfully.")
+        End If
+    End Sub
+End Class
